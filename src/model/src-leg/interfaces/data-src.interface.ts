18efb914f19348b009778ce1a78b10edb3830e97
Maciej Żółtowski <maciej.zoltowski@e-qsg.com>
2019-05-30 12:31:09 +0200

    add interface

---
+++ src/model/src-leg/interfaces/data-src.interface.ts
@@ -0,0 +1,11 @@
+export interface DataSrcInterface {
+    DataSrc_ID: string;
+    Authors: string;
+    Title: string;
+    Year: string;
+    Journal: string;
+    Vol_City: string;
+    Issue_State: string;
+    Start_Page: string;
+    End_Page: string;
+}
